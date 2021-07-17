92ef458c2384f16166c54c08d7ed240129b6e61f
Vaso <debugger.i.am@mail.ru>
2021-07-17 22:37:50 +0300

    Made JS file to generate pug file with articles. Takes articles mixins dir as entry

--- articles-files-generate.js
+++ articles-files-generate.js
@@ -1,12 +1,41 @@
 const fs = require('fs');
 const articlesMixinsDir = `./src/assets/articles/`;
-const readyArtcilesFileDir = `./src/pages/articles/`;
+const readyarticlesFileDir = `./src/pages/articles/`;
 
-function getArtcilesList(directory, exclude){  
+function getArticlesList(directory, exclude){  
   let allDirectories =  fs.readdirSync(directory);
   const neededDirectories = allDirectories.filter( fileName => !fileName.includes(exclude));
   return neededDirectories;
   
 }
 
-console.log( getArtcilesList(articlesMixinsDir, 'article') );
+function GetHeaderFromFileContent(filePath){
+  const content = `${fs.readFileSync(`${filePath}`)}`;
+  const startPoint = content.indexOf(' ', content.indexOf('h1')) + 1;
+  const endPoint = content.indexOf('\n', startPoint)-1;
+  return content.substring(startPoint, endPoint);
+}
+
+let pugFileArticleContent = '';
+const articleMixinsList = getArticlesList(articlesMixinsDir, 'article');
+for (let articleName of articleMixinsList){
+  const HeaderOfArticle = GetHeaderFromFileContent(`${articlesMixinsDir}${articleName}/${articleName}.pug`);
+  console.log(HeaderOfArticle);
+  pugFileArticleContent += `
+extends ../layout.pug
+include ../../assets/articles/${articleName}/${articleName}
+append variables
+  -
+    var PageHead = {
+      title : '${HeaderOfArticle}',
+      keywords : '${HeaderOfArticle}',
+      description : '${HeaderOfArticle}',
+    }
+    
+append content
+  +${articleName}
+  `;
+  fs.writeFileSync(`${readyarticlesFileDir}${articleName}.pug`,pugFileArticleContent);
+
+  pugFileArticleContent = '';
+}
