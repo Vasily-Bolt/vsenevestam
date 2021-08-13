a0ba72d90e99978cbae7b14ba5a90c9b7305a486
Vaso <debugger.i.am@mail.ru>
2021-08-14 00:29:31 +0300

    lightened active menus and added insta link

--- src/assets/blocks/mark-menu-item/mark-menu-item.js
+++ src/assets/blocks/mark-menu-item/mark-menu-item.js
@@ -1,6 +1,9 @@
 $(()=> {
-  const catalogPath = $(location).attr('pathname');
-  $('nav').find('a').each( function(ind,elem) {
-    if ( elem.href.includes(catalogPath) ) $(elem).addClass('mark-menu-item');
-  });
+  const catalogPath = $(location).attr('pathname').substring(1);
+  if (catalogPath != '') {
+    $('nav').find('a').each( function(ind,elem) {
+      if ( elem.href.includes(catalogPath) ) $(elem).addClass('mark-menu-item');
+    });
+  }
+  
 })
