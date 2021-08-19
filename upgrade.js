efcad8d6384a6d447052ccd3ba22d1aef23cf42d
Vaso <debugger.i.am@mail.ru>
2021-08-19 08:29:56 +0300

    Font on desktop is 16px min, Top menu font smaller, Added dresses

--- upgrade.js
+++ upgrade.js
@@ -1,6 +1,7 @@
 const articlesFilesGenerateFunc = require('./articles-files-generate.js');
-// const resizeImagesFunc = require('./resize-dresses-images.js');
+const resizeImagesFunc = require('./resize-dresses-images.js');
 const dressListPugCreateFunc = require('./check-dresses-info.js');
 articlesFilesGenerateFunc;
 dressListPugCreateFunc;
+resizeImagesFunc;
 // resizeImagesFunc;
